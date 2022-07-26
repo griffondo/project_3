454a1b152f62f3df783c294d5a7355fdf232c1c3
Griffin Martin <griffondo.7@gmail.com>
2022-07-25 22:12:57 -0400

    heroku deployment initial test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main_body">
+    <div>
+      <nav class="navbar">
+        <ul class="nav_list">
+          <li><a href='#'>About</a></li>
+          <li><a href='#'>Sign In</a></li>
+          <li><a href='#'>Start Planning</a></li>
+        </ul>
+      </nav>
+
+      <section class="body">
+        <div class="title">
+
+
+
+        </div>
+          <h1 class="My_Shredule">
+            My Shredule
+          </h1>
+          <p class="paragraph_body">
+            Winter only happens once a year. <br/>
+            Don't miss out on what the mountains have to offer <br/>
+            Make the most of the season by planning and logging your stoke filled trips <br/>
+            Get out and ride. <br/>
+            Signed, <br/>
+            The Shredule Team
+          </p>
+      </section>
     </div>
-  );
-}
+    </div>
+  )
+  }
+
+// API //
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ColorModeScript, extendTheme } from "@chakra-ui/react";
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+const theme = extendTheme({ config });
+export default theme;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <>
+    {/* 👇 Here's the script */}
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
-  </React.StrictMode>
-);
+  </>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
