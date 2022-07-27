6484606f8f4e71ff9c0bd6079302cf927f203759
Griffin Martin <griffondo.7@gmail.com>
2022-07-26 22:10:04 -0400

    fixes for heroku

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const User = require('./routes/User');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const path = require("path");
+
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
@@ -17,6 +19,10 @@ app.use(bodyParser.json());
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+app.get("*",(req, res) => {
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
 app.use("/user",User);
